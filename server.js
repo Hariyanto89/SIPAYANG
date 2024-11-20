f2f58fbe5227999d92a71ce8c7b61a755584e061
Hariyanto <yantonft@gmail.com>
2024-11-20 11:09:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const pool = require('./config/database');
+
+const app = express();
+app.use(express.json());
+
+// Cek koneksi ke database
+pool.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err.stack);
+  } else {
+    console.log('Database connected');
+  }
+});
+
+// Jalankan server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
